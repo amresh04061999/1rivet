548c19cf14afb3ffc065b9192fef2e88a5456e92
Amresh Prasad <amresh1098@gmail.com>
2022-08-18 17:45:30 +0530

    complte dynamice table

--- Create-Dynamic-table/assets/javascript/javascript.js
+++ Create-Dynamic-table/assets/javascript/javascript.js
@@ -55,38 +55,62 @@ const personDetails = [
   },
 ];
 
-
-async function getuserdata(){
+async function getuserdata() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   let data = await response.json();
-  console.log(data);
-  gePersonDetils(data) }
-   getuserdata();
-   
+  // console.log(data);
+  gePersonDetils(data);
+}
+
+getuserdata();
+
 function gePersonDetils(data) {
-  console.log(data);
+  //console.log(data);
   const gettableBody = document.querySelector(".table-body");
-  data.forEach((user) => {
+  
+  const dataitem = data.map(item => {
+    return {
+      id: item.id,
+      name: item.name,
+      username: item.username,
+      email: item.email,
+      address:item.address.city,
+      street:item.address.street
+
+    }
+    
+  });
+  // console.log(dataitem);
+  // const row = document.createElement("tr");
+  // for (const value of dataitem) {
+  //   console.log(value);
+  //      col = document.createElement("td");
+  //      console.log(col);
+  //       const elm1 = dataitem.id;
+  //       console.log(elm1);
+  //         col.textContent = elm1;
+      
+  //       row.appendChild(col);
+
+  //     }
+
+    
+  //   gettableBody.appendChild(row);
+  // }
+
+  dataitem.forEach((user) => {
     const row = document.createElement("tr");
-
+    //console.log(user);
     for (const key in user) {
-      if (["id", "name", "username", "email"].includes(key)) {
-        // console.log(user);
-        const elm = user[key];
-           //console.log(elm.address);
-
-        // const elm1 = user[key];
+       //  console.log(key);
         col = document.createElement("td");
-        col.textContent = elm;
+        const elm1 = user[key];
+          col.textContent = elm1;
         row.appendChild(col);
-      }
     }
     gettableBody.appendChild(row);
   });
 }
-
-
-
 
 
 const popbutton = document.querySelector(".add-button");
@@ -102,41 +126,37 @@ popbuttondelete.addEventListener("click", function () {
 
 
 
-
-
 function myFunction() {
   let getbutton = document.querySelector(".none-record");
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
-  console.log(input);
+  //console.log(input);
   filter = input.value.toUpperCase();
-  console.log(filter);
+  //console.log(filter);
   table = document.getElementById("myTable");
-  console.log(table);
+  //console.log(table);
   tr = table.getElementsByTagName("tr");
 
-  console.log(tr);
+ // console.log(tr);
   let found = false;
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[1];
-    console.log(td);
+   // console.log(td);
 
     if (td) {
       txtValue = td.textContent || td.innerText;
-      console.log(txtValue);
+      //console.log(txtValue);
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
         found = true;
-      } 
-      else
-       {
+      } else {
         tr[i].style.display = "none";
       }
     }
     if (found) {
-      getbutton.style.display = "none";
+      getbutton.classList.add("none")
     } else {
-      getbutton.style.display = "";
+      getbutton.classList.remove("none")
     }
   }
 }
