605a108b2190ed7b82564d7b5052b9fdc8438b6e
Amresh Prasad <amresh1098@gmail.com>
2022-08-25 18:03:14 +0530

    25-8-2022

---
+++ Dashbord-with-Scss/assets/js/javascript.js
@@ -0,0 +1,23 @@
+//  aside hover
+let list =document.querySelectorAll('.navigation li');
+console.log(list);
+function activeLink(){
+    list.forEach((item) =>
+        item.classList.remove("hovered"));
+        this.classList.add("hovered");
+ 
+}
+    list.forEach((item) =>
+        item.addEventListener("mouseover",activeLink));
+
+
+        
+        // button toggle
+        let toggle = document.querySelector(".toggle");
+        let  navigation = document.querySelector(".navigation");
+        let main = document.querySelector(".main");
+        
+        toggle.onclick=function(){
+            navigation.classList.toggle("active");
+            main.classList.toggle("active");
+        }
