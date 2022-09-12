cd6704083d0c0b8827a829ad7cbb622afc31d550
amresh <amresh1098@gmail.com>
2022-09-13 00:19:32 +0530

    update data

--- angular/my-app-09.09.2022/src/app/app.component.ts
+++ angular/my-app-09.09.2022/src/app/app.component.ts
@@ -12,15 +12,16 @@ export class AppComponent {
  public fontSize:number;
  public defaultSize:number;
  public text:string
- array :any [];
+ array :any;
+
 constructor(){
 this.fontSize=50;
 this.defaultSize=20;
 this.color='red';
 this.text = ''
-this.array=['amresh','prasad'];
-
+this.array=["amresh","prasad"];
 }
+
 // two way binding
   fontsize(changeFont:string):void{
     this.fontSize=changeFont === 'dec'?this.fontSize-10:this.fontSize+10;
@@ -30,7 +31,6 @@ this.array=['amresh','prasad'];
     }
 
   }
-
   // single binding
   keyup(value:any){
     this.text = value;
@@ -44,10 +44,61 @@ this.array=['amresh','prasad'];
  }
 
 //  add data in two way bindin
+newarray:any=[];
 add(add:string){
- this.array.push(add);
+ this.newarray.push(add);
 
+}
+// create array
+arraydata: any[] = [
+  {
+    "name": "Avengers"
+  },
+  {
+    "name": "Good Boys"
+  },
+  {
+    "name": "Playmobil"
+  },
+  {
+    "name": "Aquarela"
+  },
+  {
+    "name": "Aladdin"
+  }, 
+  {
+    "name": "Downton Abbey"
+  }
+];
 
+// Employee Data
+ Employee:any[]= [
+    {"id":"1","employee_name":"WdqBvFe","employee_salary":"797","employee_age":"36","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1925","employee_name":"Menaka6","employee_salary":"24501","employee_age":"24501","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1969","employee_name":"2381","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1970","employee_name":"6132","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1972","employee_name":"2022","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1973","employee_name":"4604","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1976","employee_name":"Shylu","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1977","employee_name":"8221","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1981","employee_name":"111test","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1996","employee_name":"test-709","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1997","employee_name":"test-654","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"1999","employee_name":"test-127","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"2001","employee_name":"test-301","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"},
+    {"id":"2003","employee_name":"1769","employee_salary":"123","employee_age":"23","profile_image":"https://img.freepik.com/free-vector/cute-koala-with-cub-cartoon-icon-illustration_138676-2839.jpg?w=2000"}
+ ]
+
+//  Delete(){
+//   this.Employee.splice(0,1);
+//  }
+ Delete(element: number) {
+  console.log(element)
+  this.Employee.forEach((value,id)=>{
+    console.log
+    if(value.id==element) this.Employee.splice(id,1);
+});
+ 
 }
 
 }
